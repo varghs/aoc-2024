a7fa3b86c6ddf72d1395bbb7b492fb267fca169e
Shawn Varghese <svarghs8531@gmail.com>
2024-12-04 16:48:13 -0500

    more code for stuff but idek know where to put it.

--- src/day03.rs
+++ src/day03.rs
@@ -77,3 +77,71 @@ pub fn part2(s: &str) -> impl Display {
 //                 if check_xmas_matrix(matrix):
 //                     new_total += 1
 //     print(new_total)
+
+// python code for day 3 of last year
+// import re
+
+// class Coordinate:
+//     def __init__(self, row, x_start, x_end, total_rows, total_cols):
+//         self.row = row
+//         self.x_start = x_start
+//         self.x_end = x_end
+//         self.total_rows = total_rows
+//         self.total_cols = total_cols
+    
+//     def check_boundary(self, lines):
+//         coords = []
+//         if self.row > 0:
+//             coords += [(self.row-1, i) for i in range(max(0, self.x_start-1), min(total_cols, self.x_end+2))]
+//         if self.x_start > 0:
+//             coords += [(self.row, self.x_start-1)]
+//         if self.x_end < self.total_cols - 1:
+//             coords += [(self.row, self.x_end+1)]
+//         if self.row < self.total_rows-1:
+//             coords += [(self.row+1, i) for i in range(max(0, self.x_start-1), min(total_cols, self.x_end+2))]
+
+//         for x, y in coords:
+//             if lines[x][y] != ".":
+//                 return True
+            
+//         return False
+
+// class ActualCoordinate:
+//     def __init__(self, x, y):
+//         self.x = x
+//         self.y = y
+    
+//     def in_range(self, other_x, other_y_start, other_y_end):
+//         return self.x == other_x and self.y >= other_y_start and self.y <= other_y_end
+
+//     def boundary_coords(self, total_rows, total_cols):
+//         coords = []
+//         for i in range(max(0, self.x-1), min(self.x+2, to))
+
+// with open("input.txt", "r") as file:
+//     text = file.read()
+//     lines = text.split("\n")
+//     matches = []
+//     pattern = r"\d+"
+//     total_rows = len(lines)
+//     total_cols = len(lines[0])
+//     for row_num, line in enumerate(lines):
+//         row_matches = re.finditer(pattern, line)
+//         for match in row_matches:
+//             start, end = match.start(), match.end() - 1
+//             num = int(match.group())
+//             matches.append((Coordinate(row_num, start, end, total_rows, total_cols), num))
+    
+//     sum = 0
+//     for match in matches:
+//         if match[0].check_boundary(lines):
+//             sum += match[1]
+    
+//     print(sum)
+
+//     gears = []
+//     for i in range(len(lines)):
+//         for j in range(len(lines[0])):
+//             if lines[i][j] == "*":
+//                 gears.append(ActualCoordinate(j, i))
+    
